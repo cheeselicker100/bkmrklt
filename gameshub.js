3bb980037a90ff6f55698c63468eed96fd9c6a8d
cheeselicker100 <117392273+cheeselicker100@users.noreply.github.com>
2022-11-07 07:59:42 -0600

    Create gameshub.js

---
+++ gameshub.js
@@ -0,0 +1,1 @@
+javascript:(function(){ window.open( 'https://sites.google.com/view/theadvancedmethod/webgl?cht=qr&chs=512x512&chl=%27 + document.location.href,%27qrcodepopup%27,%27width=700,height=700%27) })();
