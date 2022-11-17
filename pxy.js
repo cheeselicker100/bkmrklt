4bae197c635c20fb60a38d425cdc69a322919282
cheeselicker100 <117392273+cheeselicker100@users.noreply.github.com>
2022-11-17 08:01:41 -0600

    Create pxy.js

---
+++ pxy.js
@@ -0,0 +1,1 @@
+javascript:var strWindowFeatures = "location=yes,height=1280,width=720,scrollbars=yes,status=yes";var URL = "https://a.tb52ebklo.repl.co/"; var win = window.open(URL, "_blank", strWindowFeatures);
